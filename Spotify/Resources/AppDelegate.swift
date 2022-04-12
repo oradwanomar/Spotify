5eadcd19e802cd98ab51feef598f874099fca5b4
oradwanomar <oradwan037@gmail.com>
2022-04-12 07:40:43 +0200

    init folders

--- Spotify/Resources/AppDelegate.swift
+++ Spotify/Resources/AppDelegate.swift
@@ -12,9 +12,15 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = TabBarViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
         return true
     }
 
