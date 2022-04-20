39b67e9a2e5ac0218572048446b2a9332810a1f0
oradwanomar <oradwan037@gmail.com>
2022-04-20 09:16:22 +0200

    edit commit

--- Spotify/Controllers/Core/TabBarViewController.swift
+++ Spotify/Controllers/Core/TabBarViewController.swift
@@ -11,19 +11,36 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let homeVC = HomeViewController()
+        let searchVC = SearchViewController()
+        let libraryVC = LibraryViewController()
+        
+        homeVC.title = "Home"
+        searchVC.title = "Search"
+        libraryVC.title = "Library"
+        
+        homeVC.navigationItem.largeTitleDisplayMode = .always
+        searchVC.navigationItem.largeTitleDisplayMode = .always
+        libraryVC.navigationItem.largeTitleDisplayMode = .always
+        
+        let nav1 = UINavigationController(rootViewController: homeVC)
+        let nav2 = UINavigationController(rootViewController: searchVC)
+        let nav3 = UINavigationController(rootViewController: libraryVC)
+        
+        nav1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "music.note.house.fill"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "music.note.list"), tag: 3)
+        
+        
+        nav1.navigationBar.prefersLargeTitles = true
+        nav2.navigationBar.prefersLargeTitles = true
+        nav3.navigationBar.prefersLargeTitles = true
+        
+        setViewControllers([nav1,nav2,nav3], animated: false)
 
-        // Do any additional setup after loading the view.
+
+
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
